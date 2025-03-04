d6582eb1c0ee7530fecb9fac8fec08a94fc90673
Sobolev.sa <Sobolev.sa@zhcom.ru>
2025-03-04 13:53:14 +0300

    1.2.2

--- SSDK/SSDK_Data.cs
+++ SSDK/SSDK_Data.cs
@@ -8,7 +8,7 @@ namespace SSDK
 {
     public class SSDK_Data
     {
-        public static string plugin_Version = " Версия: 1.2.1";
+        public static string plugin_Version = " Версия: 1.2.2";
         
         public static bool licenses_Connection = false;
         
